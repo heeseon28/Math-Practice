70d95ab433adcade8852a7a57a75c30b143e4f12
heeseon <hgbell28@kentech.ac.kr>
2026-08-31 04:37:55 +0000

    Fixed File added

---
+++ math-practice-2026/7/sorting.py
@@ -0,0 +1,17 @@
+import logging
+import copy
+
+def bubble(lst):
+    logging.debug("Bubble sort function")
+    pass
+
+def insertion(lst):
+    logging.debug("Insertion sort function")
+    pass
+
+def merge(l1, l2):
+    logging.debug("Merge sort function")
+    pass
+
+def mergesort(lst):
+    pass
